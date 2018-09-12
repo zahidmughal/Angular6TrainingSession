ff21f383d9c28ee18a8354ed2f1a6ad320c4defd
SYSTEMS\usman.mughal <usman.mughal@systemsltd.com>
2018-09-12 15:42:31 +0500

    Changed text in file

--- src/app/Heroes/mock-hero.ts
+++ src/app/Heroes/mock-hero.ts
@@ -1,3 +1,3 @@
 import {Hero } from '../Heroes/hero'
 
-export const HEROES : Hero[] = [{ id : 1 , name : "Usman Mughal"} ,{ id : 2 , name : "Usman Ali"} ];
+export const HEROES : Hero[] = [{ id : 1 , name : "Muhammad Usman Mughal"} ,{ id : 2 , name : "Usman Ali"} ];
